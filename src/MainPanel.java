916a06bb368a780edfacc4387075ca9f8dd7e678
miaosun <miaosun88@gmail.com>
2015-01-23 19:08:42 +0000

    code cleaning

--- src/MainPanel.java
+++ src/MainPanel.java
@@ -1,11 +1,5 @@
 import java.awt.BorderLayout;
-import java.awt.Color;
-
-import javax.swing.BorderFactory;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.border.TitledBorder;
 
 
 public class MainPanel extends JPanel {
